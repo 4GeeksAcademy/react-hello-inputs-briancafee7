a478412e7e441bc7f5df17d326cd5a5a6fe77185
briancafee7-sudo <briancafee7@gmail.com>
2026-07-10 19:18:18 +0000

    primero

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue] = useState("")
+	const [peliculas, setPeliculas] = useState([])
+	const [hover, setHover] = useState(null)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	return (
+		<div className="bg-dark padre">
+			<h1 className="titulo">Guarda tus películas favoritas</h1>
+			<div className="input-group input-group-sm mb-3 inputStyle">
+				<input id="input" type="text" className="form-control" aria-label="Sizing example input"
+					aria-describedby="inputGroup-sizing-sm" onChange={e => setInputValue(e.target.value)}
+					value={inputValue} onKeyDown={e => {
+						if (e.key === "Enter") {
+							setPeliculas([...peliculas, inputValue])
+							setInputValue("")
+
+						}
+					}
+					}
+				/>
+
+			</div>
+			<ul>
+				{
+					peliculas.map((pelicula, index) => (
+						<li
+							key={index}
+							className="text-white fs-1 fw-bold hover-effect"
+							onMouseEnter={() => setHover(index)}
+							onMouseLeave={() => setHover(null)}
+							style={{
+								backgroundColor: hover === index ? "#444" : "#222",
+								transition: "0.3s",
+								listStyle: "none"
+							}}
+						>
+							{pelicula}
+						</li>
+					))
+				}
+			</ul>
+
 		</div>
+
 	);
 };
 
